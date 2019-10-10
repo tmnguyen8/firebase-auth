9f8eb29e4d15abe6287cdeb9aace6501154ddc61
Trai Nguyen <tnguyen@at-pac.com>
2019-10-10 09:41:44 -0400

    finish lesson 9

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,18 @@
+// get data
+db.collection("guides").get().then(snapshot => {
+    setupGuides(snapshot.docs);
+});
+
+// Listen for auth status changes
+auth.onAuthStateChanged(user => {
+    // console.log(user)
+    if (user) {
+        console.log('user is logged in: ', user);
+    } else {
+        console.log('user is logged out: ');
+    }
+});
+
 // sign up
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener('submit', (e) => {
@@ -8,9 +23,39 @@ signupForm.addEventListener('submit', (e) => {
 
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
+       
         const modal = document.querySelector("#modal-signup");
         M.Modal.getInstance(modal).close(); // close the modal after user is signed up
         signupForm.reset();
     });
+
 });
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut().then(() =>{
+        // console.log('user signed out');
+    });
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
+
+    // log in the user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred.user);
+        // close the login model and reset the form
+        const modal = document.querySelector("#modal-login");
+        M.Modal.getInstance(modal).close(); // close the modal after user is signed up
+        loginForm.reset();
+    })
+
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+const guideList = document.querySelector(".guides");
+
+// set up guides
+const setupGuides = (data) => {
+  let html = '';
+  data.forEach(doc => {
+    const guide = doc.data();
+    // console.log(guide)
+    const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+      </li>
+    `;
+    html += li;
+    guideList.innerHTML = html;
+
+  });
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -8,3 +28,8 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Collapsible.init(items); //materialize library model intilize modals
   
   });
+
+  // <li>
+  //           <div class="collapsible-header grey lighten-4">Guide title</div>
+  //           <div class="collapsible-body white">Lorem ipsum dolor sit amet.</div>
+  //         </li>
